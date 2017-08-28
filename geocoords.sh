098c7deeb1b5b3c9a8ed9b7966f2963c4c5d13b6
rhdzmota <rhdzmota@mxquants.com>
2017-08-28 16:17:45 -0500

    fix typo

--- geocoords.sh
+++ geocoords.sh
@@ -8,12 +8,13 @@ do
   position=$(termux-location)
   day=$(date "+%d-%m-%Y")
   time=$(date "+%H:%M:%S")
-  lat=$(echo $loc | jq .latitude)
-  lon=$(echo $loc | jq .longitude)
-  alt=$(echo $loc | jq .altitude)
-  datapoint=$(echo ${day},${time},${lon},${lat},${alt})
+  lat=$(echo $position | jq .latitude)
+  lon=$(echo $position | jq .longitude)
+  alt=$(echo $position | jq .altitude)
+  speed=$(echo $position | jq .speed)
+  datapoint=$(echo ${day},${time},${lon},${lat},${alt},${speed})
   echo $datapoint >> data/geo_data.csv
-  echo {lat:$lat, lon:$lon, alt:$alt}
+  echo {lat:$lat, lon:$lon, alt:$alt, speed:$speed}
   echo
   i=$((i+1))
 done
